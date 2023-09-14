c6bb927946eed69776db662cba98225a6bf2271a
hallashuli <hallaradwanshuli@gmail.com>
2023-09-14 15:00:29 +0300

    hhghi

--- app/src/main/java/com/example/firststephallapp/MainActivity.java
+++ app/src/main/java/com/example/firststephallapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.firststephallapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -12,5 +14,43 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("HL","onCreate");
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onRestart()
+    {
+        super.onRestart();
+        Log.d("HL","onCreate");
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        Log.d("HL","onCreate");
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        Log.d("HL","onCreate");
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        Log.d("HL","onCreate");
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        Log.d("HL","onCreate");
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
     }
 }
