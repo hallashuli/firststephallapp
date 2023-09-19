a4054ddfcdf9cade69300124c517f804351a05ca
hallashuli <hallaradwanshuli@gmail.com>
2023-09-19 14:59:31 +0300

    hhghi

--- app/src/main/java/com/example/firststephallapp/MainActivitysingin.java
+++ app/src/main/java/com/example/firststephallapp/MainActivitysingin.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Mainact2 extends AppCompatActivity {
+public class MainActivitysingin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mainact2);
+        setContentView(R.layout.activity_main_activitysingin);
     }
 }
