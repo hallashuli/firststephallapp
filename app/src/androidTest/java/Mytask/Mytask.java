9ec84a0015cc0b133a406290b5d02a66523a8fdb
hallashuli <hallaradwanshuli@gmail.com>
2023-09-20 13:56:24 +0300

    taskapp

--- app/src/androidTest/java/Mytask/Mytask.java
+++ app/src/androidTest/java/Mytask/Mytask.java
@@ -1,65 +1,71 @@
 package Mytask;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+@Entity
 public class Mytask
 {
-    public String koyld;
+    @PrimaryKey(autoGenerate = true)
+    /*** رقم المهمة*/
+    public long koyld;
+    /*** نص المهمة*/
     public String text;
+    /*** رجة الاهمية*/
     public int inportance;
+    /*** رقم موضوع المهمة*/
     public long subjid;
+    /*** رقم المستعمل*/
     public long userid;
+    /*** زمن بناء المهمة*/
     public long time;
 // gitter
     public String getKoyld() {
         return koyld;
     }
-
     public String getText() {
         return text;
     }
-
     public int getInportance() {
         return inportance;
     }
-
     public long getSubjid() {
         return subjid;
     }
-
     public long getUserid() {
         return userid;
     }
-
     public long getTime() {
         return time;
     }
 // setter
-    public void setKoyld(String koyld)
-    {
+    public void setKoyld(String koyld) {
         this.koyld = koyld;
     }
-
-    public void setText(String text)
-    {
+    public void setText(String text) {
         this.text = text;
     }
-
-    public void setInportance(int inportance)
-    {
+    public void setInportance(int inportance) {
         this.inportance = inportance;
     }
-
-    public void setSubjid(long subjid)
-    {
+    public void setSubjid(long subjid) {
         this.subjid = subjid;
     }
-
-    public void setUserid(long userid)
-    {
+    public void setUserid(long userid) {
         this.userid = userid;
     }
-
-    public void setTime(long time)
-    {
+    public void setTime(long time) {
         this.time = time;
+    }
+    @Override
+    public String toString()
+    {
+        return "Mytask{" +
+                "koyld='" + koyld + '\'' +
+                ", text='" + text + '\'' +
+                ", inportance=" + inportance +
+                ", subjid=" + subjid +
+                ", userid=" + userid +
+                ", time=" + time +
+                '}';
     }
 }
---
+++ app/src/androidTest/java/Mytask/Mytasknew.java
@@ -0,0 +1,6 @@
+package Mytask;
+
+public interface Mytasknew
+{
+
+}
