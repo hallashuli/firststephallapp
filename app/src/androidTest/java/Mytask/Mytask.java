940e1612c15e9762aed37620752bcf61b236b798
hallashuli <hallaradwanshuli@gmail.com>
2023-09-20 13:35:22 +0300

    hhghi

--- app/src/androidTest/java/Mytask/Mytask.java
+++ app/src/androidTest/java/Mytask/Mytask.java
@@ -8,8 +8,58 @@ public class Mytask
     public long subjid;
     public long userid;
     public long time;
-
+// gitter
     public String getKoyld() {
         return koyld;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public int getInportance() {
+        return inportance;
+    }
+
+    public long getSubjid() {
+        return subjid;
+    }
+
+    public long getUserid() {
+        return userid;
+    }
+
+    public long getTime() {
+        return time;
+    }
+// setter
+    public void setKoyld(String koyld)
+    {
+        this.koyld = koyld;
+    }
+
+    public void setText(String text)
+    {
+        this.text = text;
+    }
+
+    public void setInportance(int inportance)
+    {
+        this.inportance = inportance;
+    }
+
+    public void setSubjid(long subjid)
+    {
+        this.subjid = subjid;
+    }
+
+    public void setUserid(long userid)
+    {
+        this.userid = userid;
+    }
+
+    public void setTime(long time)
+    {
+        this.time = time;
     }
 }
