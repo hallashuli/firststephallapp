6b525bfa78c22f7835103ef75ccba48242a733bb
hallashuli <hallaradwanshuli@gmail.com>
2023-10-02 14:11:09 +0300

    taskapp

--- app/src/main/java/com/example/firststephallapp/MainActivitysingin.java
+++ app/src/main/java/com/example/firststephallapp/MainActivitysingin.java
@@ -3,12 +3,14 @@ package com.example.firststephallapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivitysingin extends AppCompatActivity {
     private EditText etEmail;
     private EditText etPass;
-
+    private Button btSignin;
+    private Button btSingup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
