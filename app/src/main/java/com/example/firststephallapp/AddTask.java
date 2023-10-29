d39ba68dcf771aa0ff598222c8dc7745387e5896
halla <shulihalla@gmail.com>
2023-10-29 12:23:32 +0200

    addTask class done

--- app/src/main/java/com/example/firststephallapp/AddTask.java
+++ app/src/main/java/com/example/firststephallapp/AddTask.java
@@ -1,6 +1,22 @@
 package com.example.firststephallapp;
 
-public class AddTask
-{
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.textfield.TextInputLayout;
 
+public class AddTask extends AppCompatActivity {
+    private Button btn2Save;
+    private Button btn2Cancel;
+    private EditText etImportance;
+    private SeekBar skb;
+    private TextInputLayout etShortTitle;
+    private TextInputLayout etText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addtask);
+    }
 }
