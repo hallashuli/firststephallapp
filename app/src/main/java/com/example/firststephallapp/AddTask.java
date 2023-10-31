0b498b39b1f6ef2512a331b35b8aeac19dee9efc
halla <shulihalla@gmail.com>
2023-10-31 11:40:00 +0200

    comp

--- app/src/main/java/com/example/firststephallapp/AddTask.java
+++ app/src/main/java/com/example/firststephallapp/AddTask.java
@@ -2,21 +2,30 @@ package com.example.firststephallapp;
 
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.SeekBar;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.textfield.TextInputLayout;
 
 public class AddTask extends AppCompatActivity {
     private Button btn2Save;
     private Button btn2Cancel;
-    private EditText etImportance;
+    private TextView etImportance;
     private SeekBar skb;
     private TextInputLayout etShortTitle;
     private TextInputLayout etText;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addtask);
+        //
+        btn2Cancel=findViewById(R.id.btn2Cancel);
+        btn2Save=findViewById(R.id.btn2Save);
+        etImportance=findViewById(R.id.etImportance);
+        skb=findViewById(R.id.skb);
+        etShortTitle=findViewById(R.id.etShortTitle);
+        etText=findViewById(R.id.etText);
     }
 }
--- app/src/main/java/com/example/firststephallapp/EditTask.java
+++ app/src/main/java/com/example/firststephallapp/EditTask.java
@@ -1,15 +1,16 @@
 package com.example.firststephallapp;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.SeekBar;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.textfield.TextInputLayout;
 public class EditTask extends AppCompatActivity
 {
     private Button btnUpdate;
     private Button btn3Cancel;
-    private EditText etImportance2;
+    private TextView etImportance2;
     private SeekBar skb2;
     private TextInputLayout etShortTitle2;
     private TextInputLayout etText2;
@@ -21,8 +22,8 @@ public class EditTask extends AppCompatActivity
         //
         btnUpdate=findViewById(R.id.btnUpdate);
         btn3Cancel=findViewById(R.id.btn3Cancel);
-        etImportance2=findViewById(R.id.etImportance2);
-        skb2=findViewById(R.id.skb2);
+        etImportance2=findViewById(R.id.etImportance);
+        skb2=findViewById(R.id.skb);
         etShortTitle2=findViewById(R.id.etShortTitle2);
         etText2=findViewById(R.id.etText2);
 
